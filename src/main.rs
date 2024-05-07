00f81c1b340e02a0ddeeeaeaec0b48b6d0ad3133
adhis <tiva.adhisti@ui.ac.id>
2024-05-07 23:28:23 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- src/main.rs
+++ src/main.rs
@@ -104,12 +104,24 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Adhis's Computer: done!");
     });
+    spawner.spawn(async {
+        println!("Adhis's Computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Adhis's Computer: done2!");
+    });
+    spawner.spawn(async {
+        println!("Adhis's Computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Adhis's Computer: done3!");
+    });
 
     println!("Adhis's Computer: hey hey");
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
